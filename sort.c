2cb4135564722c77bb45ae67b13e38b0e97b8ce6
DogTailGrass <18761880125@163.com>
2020-07-02 13:55:42 +0800

    Update sort.c

--- sort.c
+++ sort.c
@@ -3,7 +3,7 @@
 
 int a[1000];
 
-void DirectSort(int start,int end)
+void DirectInsertSort(int start,int end)
 {
 	int i,j;
 	for (i = start + 1; i <= end; ++i) 
@@ -13,11 +13,11 @@ void DirectSort(int start,int end)
             int temp = a[i];
             j = i - 1;
             do 
-			{    //ÒÀ´ÎÒÆ¶¯²¢Ñ°ÕÒ²åÈëÎ»ÖÃ
+			{    //ï¿½ï¿½ï¿½ï¿½ï¿½Æ¶ï¿½ï¿½ï¿½Ñ°ï¿½Ò²ï¿½ï¿½ï¿½Î»ï¿½ï¿½
                 a[j + 1] = a[j];
                 --j;
             }while (j >= start && a[j] > temp);
-            a[j + 1] = temp;  //²åÈëÔªËØ
+            a[j + 1] = temp;  //ï¿½ï¿½ï¿½ï¿½Ôªï¿½ï¿½
         }
     }
 	
@@ -83,33 +83,30 @@ void bubbsort()
 }
  
  int main()
- {
- 	int c = 1;
- 	int b = 2;
- 	swap(&b,&c);
- 	printf("%d\n",b);
- 	printf("%d\n",c);
- 	
+ { 	
  	int i = 0;
  	produceRandomNumbers(20);
  	for(i=0;i<20;i++)
  	{
  		printf("%d\n",a[i]);
 	}
+	#if 0
 	printf("\n----------------------------------\n");
-	 
-	//sort();
 	bubbsort();
 	for(i=0;i<20;i++)
  	{
  		printf("%d\n",a[i]);
 	}
+	#endif
+
+	#if 0
 	printf("\n----------------------------------\n");
-	//DirectSort(0,19);
-	//for(i=0;i<20;i++)
- 	//{
- 	//	printf("%d\n",a[i]);
-	//}
+	DirectInsertSort(0,19);
+	for(i=0;i<20;i++)
+ 	{
+ 		printf("%d\n",a[i]);
+	}
+	#endif
 	while(1)
 	{
 	}
