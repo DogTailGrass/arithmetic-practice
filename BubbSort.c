247c710e9b5a2a4ce47bfff3a7eb235e6cd73942
DogTailGrass <18761880125@163.com>
2020-07-02 15:13:51 +0800

    冒泡排序

--- BubbSort.c
+++ BubbSort.c
@@ -3,49 +3,6 @@
 
 int a[1000];
 
-void DirectInsertSort1(int start,int end)
-{
-	int i,j;
-	for (i = start + 1; i <= end; ++i) 
-	{
-        if (a[i] < a[i - 1]) 
-		{
-            int temp = a[i];
-            j = i - 1;
-            do 
-			{    
-                a[j + 1] = a[j];
-                --j;
-            }while (j >= start && a[j] > temp);
-            a[j + 1] = temp;
-        }
-    }
-	
-	return;
- } 
- 
-void DirectInsertSort2(int start,int len)
-{
-	int temp,k,j,i;
-	for(i=start+1; i < len; i++)
-	{
-		temp = a[i];
-		j = i;
-		while((temp < a[j-1]) && (j>=1))
-		{
-			j --;
-		}
-		
-		for(k=i-1;k>=j;k--)
-		{
-			a[k+1]=a[k];
-		}
-		a[j]=temp;
-	}
-	return;
-	
-}
-
 void swap(int *a,int *b)
 {
 	int temp;
@@ -90,32 +47,12 @@ void bubbsort()
  	{
  		printf("%d\n",a[i]);
 	}
-	#if 0
 	printf("\n----------------------------------\n");
 	bubbsort();
 	for(i=0;i<20;i++)
  	{
  		printf("%d\n",a[i]);
 	}
-	#endif
-	
-	#if 0
-	printf("\n----------------------------------\n");
-	DirectInsertSort1(0,19);
-	for(i=0;i<20;i++)
- 	{
- 		printf("%d\n",a[i]);
-	}
-	#endif
-
-	#if 1
-	printf("\n----------------------------------\n");
-	DirectInsertSort2(0,20);
-	for(i=0;i<20;i++)
- 	{
- 		printf("%d\n",a[i]);
-	}
-	#endif
 	while(1)
 	{
 	}
