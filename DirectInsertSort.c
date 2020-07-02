ad904c3da3a3feec32df00412066c4db6aeb9687
DogTailGrass <18761880125@163.com>
2020-07-02 15:14:36 +0800

    直接插入排序，两种写法

--- DirectInsertSort.c
+++ DirectInsertSort.c
@@ -46,32 +46,6 @@ void DirectInsertSort2(int start,int len)
 	
 }
 
-void swap(int *a,int *b)
-{
-	int temp;
-	temp=*a;
-	*a = *b;
-	*b = temp;
-}
-
-void bubbsort()
-{
-	int i,j;
-	int temp;
-	
-	for(i=19;i>=0;i--)
-	{
-		for(j=i;j>0;j--)
-		{
-			if(a[j]<a[j-1])
-			{
-				swap(&a[j],&a[j-1]);
-			}
-		}
-	}
-	return;
-}
- 
  void produceRandomNumbers(int count) 
 {
    int i=0;
@@ -90,14 +64,6 @@ void bubbsort()
  	{
  		printf("%d\n",a[i]);
 	}
-	#if 0
-	printf("\n----------------------------------\n");
-	bubbsort();
-	for(i=0;i<20;i++)
- 	{
- 		printf("%d\n",a[i]);
-	}
-	#endif
 	
 	#if 0
 	printf("\n----------------------------------\n");
